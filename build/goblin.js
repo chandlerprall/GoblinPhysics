7eebea214849ecdd20301c11323ea2447b00b0e7
Chandler Prall <chandler@sympoz.com>
2014-04-11 17:46:04 -0600

    Complete [hopefully] raytracing against cones

--- build/goblin.js
+++ build/goblin.js
@@ -4089,14 +4089,23 @@ Goblin.ConeShape = function( radius, half_height ) {
     this.aabb = new Goblin.AABB();
     this.calculateLocalAABB( this.aabb );
 
-	/**
-	 * sin of the cone's angle
-	 *
-	 * @property _sinagle
-	 * @type {Number}
-	 * @private
-	 */
+    /**
+     * sin of the cone's angle
+     *
+     * @property _sinagle
+     * @type {Number}
+     * @private
+     */
 	this._sinangle = this.radius / Math.sqrt( this.radius * this.radius + Math.pow( 2 * this.half_height, 2 ) );
+
+    /**
+     * cos of the cone's angle
+     *
+     * @property _cosangle
+     * @type {Number}
+     * @private
+     */
+    this._cosangle = Math.cos( Math.asin( this._sinangle ) );
 };
 
 /**
@@ -4165,114 +4174,193 @@ Goblin.ConeShape.prototype.findSupportPoint = function( direction, support_point
  * @return {RayIntersection|null} if the segment intersects, a RayIntersection is returned, else `null`
  */
 Goblin.ConeShape.prototype.rayIntersect = (function(){
-	var direction = vec3.create(),
-		length;
+    var direction = vec3.create(),
+        length,
+        p1 = vec3.create(),
+        p2 = vec3.create();
 
-	return function( start, end ) {
-		vec3.subtract( end, start, direction );
-		length = vec3.length( direction );
-		vec3.scale( direction, 1 / length ); // normalize direction
+    return function( start, end ) {
+        vec3.subtract( end, start, direction );
+        length = vec3.length( direction );
+        vec3.scale( direction, 1 / length ); // normalize direction
 
-		var cosangle = Math.cos( Math.asin( this._sinangle ) ),
-			point = null;
+        var t1, t2;
 
-		var A = vec3.createFrom( 0, -1, 0 );
+        // Check for intersection with cone base
+        t1 = this._rayIntersectBase( start, end, p1 );
 
-		var AdD = vec3.dot( A, direction ),
-			cosSqr = cosangle * cosangle;
+        // Check for intersection with cone shape
+        t2 = this._rayIntersectCone( start, direction, length, p2 );
 
-		var E = vec3.create();
-		E[0] = start[0];
-		E[1] = start[1] - this.half_height;
-		E[2] = start[2];
+        var intersection;
 
-		var AdE = vec3.dot( A, E ),
-			DdE = vec3.dot( direction, E ),
-			EdE = vec3.dot( E, E ),
-			c2 = AdD * AdD - cosSqr,
-			c1 = AdD * AdE - cosSqr * DdE,
-			c0 = AdE * AdE - cosSqr * EdE,
-			dot, t;
+        if ( !t1 && !t2 ) {
+            return null;
+        } else if ( !t2 || ( t1 < t2 ) ) {
+            intersection = Goblin.ObjectPool.getObject( 'RayIntersection' );
+            intersection.object = this;
+            vec3.set( p1, intersection.point );
+            return intersection;
+        } else if ( !t1 || ( t2 < t1 ) ) {
+            intersection = Goblin.ObjectPool.getObject( 'RayIntersection' );
+            intersection.object = this;
+            vec3.set( p2, intersection.point );
+            return intersection;
+        }
 
-		if ( Math.abs( c2 ) >= Goblin.EPSILON ) {
-			var discr = c1 * c1 - c0 * c2;
-			if ( discr < 0 ) {
-				return null;
-			} else if ( discr > Goblin.EPSILON ) {
-				var root = Math.sqrt( discr ),
-					invC2 = 1 / c2,
-					tmin;
+        return null;
+    };
+})();
 
-				tmin = t = ( -c1 - root ) * invC2;
-				if ( tmin >= 0 && tmin <= length ) {
-					point = vec3.create();
-					vec3.scale( direction, t, point );
-					vec3.add( point, start );
-					E[1] = point[1] - this.half_height;
-					dot = vec3.dot( E, A );
-					if ( dot < 0 ) {
-						point = null;
-					}
-				}
+Goblin.ConeShape.prototype._rayIntersectBase = (function(){
+    var normal = vec3.createFrom( 0, -1, 0 ),
+        ab = vec3.create(),
+        _start = vec3.create(),
+        _end = vec3.create(),
+        t;
 
-				t = ( -c1 + root ) * invC2;
-				if ( t >= 0 && t <= length ) {
-					if ( t < tmin || point == null ) {
-						tmin = t;
-						point = vec3.create();
-						vec3.scale( direction, t, point );
-						vec3.add( point, start );
-						E[1] = point[1] - this.half_height;
-						dot = vec3.dot( E, A );
-						if ( dot < 0 ) {
-							point = null;
-						}
-					}
-				}
+    return function( start, end, point ) {
+        _start[0] = start[0];
+        _start[1] = start[1] + this.half_height;
+        _start[2] = start[2];
 
-				if ( point == null ) {
-					return null;
-				}
-			} else {
-				t = c1 / c2;
-				point = vec3.create();
-				vec3.scale( direction, t, point );
-				vec3.subtract( start, point, point );
-				E[1] = point[1] - this.half_height;
-				dot = vec3.dot( E, A );
-				if ( dot < 0 ) {
-					return null;
-				}
+        _end[0] = end[0];
+        _end[1] = end[1] + this.half_height;
+        _end[2] = end[2];
 
-				// Verify segment reaches point
-				vec3.subtract( point, start, _tmp_vec3_1 );
-				if ( vec3.squaredLength( _tmp_vec3_1 ) > length * length ) {
-					return null;
-				}
-			}
-		} else if ( Math.abs( c1 ) >= Goblin.EPSILON ) {
-			t = 0.5 * c0 / c1;
-			point = vec3.create();
-			vec3.scale( direction, t, point );
-			vec3.add( point, start );
-			E[1] = point[1] - this.half_height;
-			dot = vec3.dot( E, A );
-			if ( dot < 0 ) {
-				return null;
-			}
-		} else if ( Math.abs( c0 ) >= Goblin.EPSILON) {
-			return null;
-		} else {
-			point = vec3.create();
-			point[1] = this.half_height;
-		}
+        vec3.subtract( _end, _start, ab );
+        t = -vec3.dot( normal, _start ) / vec3.dot( normal, ab );
 
-		var intersection = Goblin.ObjectPool.getObject( 'RayIntersection' );
-		intersection.object = this;
-		vec3.set( point, intersection.point );
+        if ( t < 0 || t > 1 ) {
+            return null;
+        }
 
-		return intersection;
-	};
+        vec3.scale( ab, t, point );
+        vec3.add( point, start );
+
+        if ( point[0] * point[0] + point[2] * point[2] > this.radius * this.radius ) {
+            return null;
+        }
+
+        return t;
+    };
+})();
+
+/**
+ * Checks if a ray segment intersects with the cone definition
+ *
+ * @method _rayIntersectCone
+ * @property start {vec3} start point of the segment
+ * @property direction {vec3} normalized direction vector of the segment, from `start`
+ * @property length {Number} segment length
+ * @property point {vec3} (out) location of intersection
+ * @private
+ * @return {vec3|null} if the segment intersects, point where the segment intersects the cone, else `null`
+ */
+Goblin.ConeShape.prototype._rayIntersectCone = (function(){
+    var _point = vec3.create();
+
+    return function( start, direction, length, point ) {
+        var A = vec3.createFrom( 0, -1, 0 );
+
+        var AdD = vec3.dot( A, direction ),
+            cosSqr = this._cosangle * this._cosangle;
+
+        var E = vec3.create();
+        E[0] = start[0];
+        E[1] = start[1] - this.half_height;
+        E[2] = start[2];
+
+        var AdE = vec3.dot( A, E ),
+            DdE = vec3.dot( direction, E ),
+            EdE = vec3.dot( E, E ),
+            c2 = AdD * AdD - cosSqr,
+            c1 = AdD * AdE - cosSqr * DdE,
+            c0 = AdE * AdE - cosSqr * EdE,
+            dot, t, tmin = null;
+
+        if ( Math.abs( c2 ) >= Goblin.EPSILON ) {
+            var discr = c1 * c1 - c0 * c2;
+            if ( discr < 0 ) {
+                return null;
+            } else if ( discr > Goblin.EPSILON ) {
+                var root = Math.sqrt( discr ),
+                    invC2 = 1 / c2;
+
+                t = ( -c1 - root ) * invC2;
+                if ( t >= 0 && t <= length ) {
+                    vec3.scale( direction, t, _point );
+                    vec3.add( _point, start );
+                    E[1] = _point[1] - this.half_height;
+                    dot = vec3.dot( E, A );
+                    if ( dot >= 0 ) {
+                        tmin = t;
+                        vec3.set( _point, point );
+                    }
+                }
+
+                t = ( -c1 + root ) * invC2;
+                if ( t >= 0 && t <= length ) {
+                    if ( tmin == null || t < tmin ) {
+                        vec3.scale( direction, t, _point );
+                        vec3.add( _point, start );
+                        E[1] = _point[1] - this.half_height;
+                        dot = vec3.dot( E, A );
+                        if ( dot >= 0 ) {
+                            tmin = t;
+                            vec3.set( _point, point );
+                        }
+                    }
+                }
+
+                if ( tmin == null ) {
+                    return null;
+                }
+                tmin /= length;
+            } else {
+                t = -c1 / c2;
+                vec3.scale( direction, t, _point );
+                vec3.add( _point, start );
+                E[1] = _point[1] - this.half_height;
+                dot = vec3.dot( E, A );
+                if ( dot < 0 ) {
+                    return null;
+                }
+
+                // Verify segment reaches _point
+                vec3.subtract( _point, start, _tmp_vec3_1 );
+                if ( vec3.squaredLength( _tmp_vec3_1 ) > length * length ) {
+                    return null;
+                }
+
+                tmin = t / length;
+                vec3.set( _point, point );
+            }
+        } else if ( Math.abs( c1 ) >= Goblin.EPSILON ) {
+            t = 0.5 * c0 / c1;
+            vec3.scale( direction, t, _point );
+            vec3.add( _point, start );
+            E[1] = _point[1] - this.half_height;
+            dot = vec3.dot( E, A );
+            if ( dot < 0 ) {
+                return null;
+            }
+            tmin = t;
+            vec3.set( _point, point );
+        } else if ( Math.abs( c0 ) >= Goblin.EPSILON) {
+            return null;
+        } else {
+            console.error( 'No t value :(' );
+            point[0] = point[2] = 0;
+            point[1] = this.half_height;
+        }
+
+        if ( point[1] < -this.half_height ) {
+            return null;
+        }
+
+        return tmin;
+    };
 })();
 /**
  * @class CylinderShape
