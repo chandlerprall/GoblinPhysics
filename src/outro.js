150e2b486dcdd840d3e31fbf7383d816096dc5d6
Chandler Prall <chandler.prall@gmail.com>
2015-11-02 21:07:55 -0700

    Exports Goblin namespace to web worker's , closes #54

--- src/outro.js
+++ src/outro.js
@@ -1,3 +1,4 @@
 	if ( typeof window !== 'undefined' ) window.Goblin = Goblin;
+	if ( typeof self !== 'undefined' ) self.Goblin = Goblin;
 	if ( typeof module !== 'undefined' ) module.exports = Goblin;
 })();
